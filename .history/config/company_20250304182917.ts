8e6f78524c762c54e528b2891e9a56b45ba62fbb
Владислав <vladislav@MacBook-Air-Vladislav.local>
2025-03-05 00:12:51 +0300

    removed history

--- .history/config/company_20250304182917.ts
+++
@@ -1,9 +0,0 @@
-export const companyInfo = {
-  name: "Бегунок",
-  phone: "+7 (915) 419-08-06",
-  phoneRaw: "+79154190806", // For tel: links
-  address: "105203, РОССИЯ, Г МОСКВА, УЛ 14-Я ПАРКОВАЯ, Д 5, КВ 24",
-  telegram: "@RRR_MSK_77",
-  workingHours: "Пн-Вс: 8:00 - 22:00",
-}
-
